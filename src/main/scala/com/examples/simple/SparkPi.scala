1493c7129b77ddff068c3f66ca7c60e6006be6d0
shishishu <zhouxj_xinhua@163.com>
2020-07-06 23:53:43 +0800

    add SparkPi

---
+++ src/main/scala/com/examples/simple/SparkPi.scala
@@ -0,0 +1,21 @@
+package com.examples.simple
+
+import com.tutorial.commons.Context
+import scala.math.random
+
+object SparkPi extends Context {
+
+  def main(args: Array[String]): Unit ={
+    val slices = if (args.length > 0) args(0).toInt else 2
+    val n = math.min(100000L * slices, Int.MaxValue).toInt  // avoid overflow
+    val count = sc.parallelize(1 until n, slices)
+      .map(_ => {
+        val x = random * 2 - 1
+        val y = random * 2 - 1
+        if (x * x + y * y <= 1) 1 else 0
+      }).reduce(_ + _)
+    println(s"Pi is roughly ${4.0 * count / (n - 1)}")
+    sc.stop()
+  }
+
+}

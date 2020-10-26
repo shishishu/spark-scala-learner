4e07cb86eb0d4cbe80a859d35cdb992d4aa32b30
shishishu <zhouxj_xinhua@163.com>
2020-10-27 00:22:09 +0800

    add KMeans

---
+++ src/main/scala/com/examples/simple/KMeans.scala
@@ -0,0 +1,62 @@
+package com.examples.simple
+
+import breeze.linalg.{DenseVector, Vector, squaredDistance}
+import com.tutorial.commons.Context
+
+object KMeans extends Context {
+
+  def parseVector(line: String): Vector[Double] = {
+    DenseVector(line.split(' ').map(_.toDouble))
+  }
+
+  def closestPoint(p: Vector[Double], centers: Array[Vector[Double]]): Int = {
+    var bestIndex = 0
+    var closest = Double.PositiveInfinity
+
+    for (i <- 0 until centers.length){
+      val tmpDist = squaredDistance(p, centers(i))
+      if (tmpDist < closest){
+        closest = tmpDist
+        bestIndex = i
+      }
+    }
+    bestIndex
+  }
+
+  def main(args: Array[String]): Unit ={
+
+    val lines = sc.textFile("src/main/resources/kmeans.txt")
+    val data = lines.map(parseVector).cache()
+    val K = 2
+    val convergeDist = math.pow(10, -5)
+
+    val kPoints = Array(Vector(0.0, 0.0), Vector(3.0, 3.0))
+    var tmpDist = 1.0
+
+    while (tmpDist > convergeDist){
+      val closest = data.map(p => (closestPoint(p, kPoints), (p, 1)))
+
+      // https://stackoverflow.com/questions/30875381/using-case-in-pairrddfunctions-reducebykey
+      //Scala supports multiple ways of defining anonymous functions. The "case" version is so called Pattern Matching Anonymous Functions
+      val pointStats = closest.reduceByKey{case ((p1, c1), (p2, c2)) => (p1 + p2, c1 + c2)}
+
+      val newPoints = pointStats
+        .map(pair => (pair._1, pair._2._1 * (1.0 / pair._2._2)))
+        .collectAsMap()
+
+      tmpDist = 0.0
+      for (i <- 0 until K){
+        tmpDist += squaredDistance(kPoints(i), newPoints(i))
+      }
+
+      for (newP <- newPoints){
+        kPoints(newP._1) = newP._2
+      }
+      println(s"Finished iteration (delta = $tmpDist)")
+    }
+
+    println("Final centers:")
+    kPoints.foreach(println)
+    sc.stop()
+  }
+}
